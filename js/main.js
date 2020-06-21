78f6000a2f5143c52c9bbec992ce09205cf5292b
Ryry <r.tillman.french@gmail.com>
2020-06-21 10:50:16 -0400

    Adding PEMDAS logic

--- js/main.js
+++ js/main.js
@@ -15,45 +15,60 @@ class Calculator {
     document.querySelector('#window').innerHTML = "";
     this.currNum = "";
   }
-  run() {
-    this.equation.push(calc.currNum)
-    const firstNum = parseInt(this.equation[0])
-    const secondNum = parseInt(this.equation[2])
-    if (this.equation.length > 2){
-      switch (this.equation[1]){
+
+  pemdas(array) {
+    let equations = ["*", "/", "+", "-"]
+
+    for (let i = 0; i < equations.length; i++){
+      let equat = [array[array.indexOf(equations[i])-1],array[array.indexOf(equations[i])], array[array.indexOf(equations[i])+1]]
+      console.log(equat);
+      if (array.indexOf(equations[i]) >= 0){
+        array.splice(array.indexOf(equations[i])-1, 3, this.run(equat))
+      }
+    }
+    return this.resolve(array[0])
+  }
+
+  run(eq) {
+    const firstNum = typeof eq[0] === "string" ? parseInt(eq[0]) : eq[0]
+    const secondNum = typeof eq[2] === "string" ? parseInt(eq[2]) : eq[2]
+
+    if (eq.length > 2){
+      console.log(eq[1]);
+      switch (eq[1]){
         case '+':
-        this.add(firstNum, secondNum)
+        return this.add(firstNum, secondNum)
         break;
         case '-':
-        this.subtract(firstNum, secondNum)
+        return this.subtract(firstNum, secondNum)
         break;
         case '*':
-        this.multiply(firstNum, secondNum)
+        return this.multiply(firstNum, secondNum)
         break;
         case '/':
-        this.divide(firstNum, secondNum)
+        return this.divide(firstNum, secondNum)
         break;
       }
     }
   }
+
   add(a,b) {
-    this.resolve(a + b)
+    return (a + b)
   }
   subtract(a,b) {
-    this.resolve(a - b)
+    return (a - b)
   }
   multiply(a,b) {
-    this.resolve(a * b)
+    return (a * b)
   }
   divide(a,b) {
-    this.resolve(a / b)
+    return (a / b)
   }
   resolve(num) {
     document.querySelector('#window').innerHTML = num;
     this.complete = !this.complete
     this.equation = [];
     this.currNum = "";
-    console.log(calc);
   }
 }
 
@@ -73,10 +88,11 @@ document.querySelectorAll('button').forEach(button => {
     if (type === 'equation'){
       calc.equation.push(calc.currNum,button)
       calc.clear()
-    } else if ( type === 'number') {
+    } else if ( type === 'number' || type === 'decimal') {
       calc.getNum(button)
     } else {
-      calc.run()
+      calc.equation.push(calc.currNum)
+      calc.pemdas(calc.equation)
     }
   })
 });
